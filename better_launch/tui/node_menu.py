29f1e453c1859f4cb36c9b7dcc9ad06a9e564979
Nikolas Dahn <nikolas.dahn@dfki.de>
2025-01-31 19:36:21 +0100

    Textual is somewhat working now

--- better_launch/tui/node_menu.py
+++ better_launch/tui/node_menu.py
@@ -1,37 +1,48 @@
+from textual.binding import Binding
 from textual.widgets import ListView, ListItem, Label
+from textual.containers import VerticalGroup
+from textual.screen import ModalScreen
 
 from .node_status import NodeStatus
 
 
-class NodeMenu(ListView):
+class NodeMenu(ModalScreen):
     """Custom widget that extends Container."""
+
+    BINDINGS = [
+        Binding("esc", "dismiss")
+    ]
 
     DEFAULT_CSS = """
     NodeMenu {
+        background: rgba(0, 0, 0, 0.3);
         align: center middle;
-        width: 40;
-        padding: 2 4;
-        display: none;
+
+        ListView {
+            max_width: 30;
+            width: auto;
+            height: auto;
+            padding: 1 2;
+        }
     }
     """
-    def __init__(self, id: str = None):
-        super().__init__(*[
-            # TODO add icons
-            ListItem(Label(item))
-            for item in [
-                "toggle mute", 
-                "kill",
-            ]
-        ], id=id)
 
-        self.node = None
-
-    def show_for_node(self, node: NodeStatus):
-        # TODO update indicators 
+    def __init__(self, node, *, name = None, id = None, classes = None):
         self.node = node
-        self.display = True
+        super().__init__(name, id, classes)
+
+    def compose(self):
+        # TODO is None?
+        yield Label("Hello")
+
+        items = [
+            ListItem(Label(action)) 
+            for action in ["info", "kill"]
+        ]
+        yield ListView(*items)
 
     def on_list_view_selected(self, item: ListItem):
+        # TODO item.name is not valid for ListItem
         if item.name == "toggle mute":
             self.node.mute = not self.node.mute
         elif item.name == "kill":
@@ -41,4 +52,4 @@ class NodeMenu(ListView):
             # Unknown command
             return
 
-        self.display = False
+        self.dismiss()
