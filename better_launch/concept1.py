eeea52d388c084c86e8765f86ed7fb6039beaa93
Nikolas Dahn <nikolas.dahn@dfki.de>
2025-02-10 18:37:03 +0100

    - Inclusion of better_launch launch files now working! - Fixed regression due to previous is_running extension - ros2 aliveness now checked via Node.is_ros2_connected

--- better_launch/concept1.py
+++ better_launch/concept1.py
@@ -2,14 +2,17 @@ import os
 from better_launch import BetterLaunch, launch_this, LifecycleStage
 
 
-# NOTE arguments default to NONE if not specified
 @launch_this(ui=True)
+# All arguments will be exposed as command line args (e.g. --enable_x)
+# NOTE arguments default to NONE if not specified
 def test(enable_x: bool):
+    # The function docstring will be used to create the CLI help message
     """
     This is how nice your launch files could be!
     """
     bl = BetterLaunch()
 
+    # Regular node
     #with bl.group("test"):
     #    bl.node(
     #        "examples_rclpy_minimal_publisher",
@@ -17,11 +20,13 @@ def test(enable_x: bool):
     #        "test_node",
     #    )
 
+    # Composition
     #with bl.group("composition"):
     #    with bl.compose("composed"):
     #        bl.component("composition", "composition::Talker", "comp_talker")
     #        bl.component("composition", "composition::Listener", "comp_listener")
 
+    # Lifecycle nodes
     with bl.group("lifecycle_A"):
         bl.node(
             "lifecycle",
@@ -30,4 +35,5 @@ def test(enable_x: bool):
             lifecycle_target=LifecycleStage.PRISTINE,
         )
 
+        # Include another BetterLaunch file
         bl.include(os.path.dirname(__file__) + "/concept2.py")
