2b8d25b05cba9501e96e188e73295f6811f26291
Nikolas Dahn <nikolas.dahn@dfki.de>
2025-02-14 18:44:50 +0100

    - Added useful imports to main __init__.py - Added simple setup.py

--- better_launch/__init__.py
+++ better_launch/__init__.py
@@ -2,3 +2,12 @@ __version__ = "0.9.0"
 
 from .launcher import launch_this, BetterLaunch
 from .elements.abstract_node import LifecycleStage
+from .ros.logging import LaunchConfig as LogConfig
+from . import elements
+from . import utils
+
+__all__ = [
+    "launch_this",
+    "BetterLaunch",
+    "LifecycleStage"
+]
