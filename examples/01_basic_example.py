35bf624c14588caf52035d85ef3e659f969943cc
Nikolas Dahn <nikolas.dahn@dfki.de>
2025-02-20 17:45:41 +0100

    - Added BetterLaunch.get_ros_message_type and included it in publisher/service/action client calls - Added additional example launch file

--- examples/01_basic_example.py
+++ examples/01_basic_example.py
@@ -5,6 +5,12 @@ from better_launch import BetterLaunch, launch_this
 def test():
     """
     This is how nice your launch files could be!
+
+    You can run this launch file either directly or with the included `bl` script which should be on your PATH once you have built better_launch and sourced your workspace:
+
+    .. code:: bash
+
+        bl better_launch 01_basic_example.py
     """
     bl = BetterLaunch()
 
