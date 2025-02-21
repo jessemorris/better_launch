d7f870340a02941f59f76af4fba7251bebefe674
Nikolas Dahn <nikolas.dahn@dfki.de>
2025-02-21 20:21:54 +0100

    Extended the examples to show more of what better_launch can do

--- examples/02_ui.py
+++ examples/02_ui.py
@@ -3,13 +3,13 @@ from better_launch import BetterLaunch, launch_this
 
 # NOTE This is the new part!
 @launch_this(ui=True)
-def test():
+def a_nice_ui():
     """
     This example starts the same nodes as the previous one, but uses a terminal UI for display and management. Use the keyboard or mouse to interact with it and see what you can do! 
     """
     bl = BetterLaunch()
 
-    with bl.group("test"):
+    with bl.group("basic"):
         bl.node(
             "examples_rclpy_minimal_publisher",
             "publisher_local_function",
--- examples/03_composition.py
+++ examples/03_composition.py
@@ -1,18 +1,29 @@
 from better_launch import BetterLaunch, launch_this
+from better_launch.elements import Component
 
 
 @launch_this(ui=True)
-def test():
+def not_a_song():
     """
     This example will create a Composer node and load two example plugins into it from the composition package:
     * composition/composition::Talker
     * composition/composition::Listener
-    
-    Using the terminal UI (the TUI), you will see how they appear as a single node. Opening the node dialog from the sidebar will allow you to unload individual components or kill the entire composer.
+
+    Using the terminal UI (the TUI), you will see how they appear as a single node. Opening the node dialog from the sidebar will also allow you to unload individual components or kill the entire composer.
     """
     bl = BetterLaunch()
 
-    with bl.group("composition"):
-        with bl.compose("my_composer"):
-            bl.component("composition", "composition::Talker", "comp_talker")
-            bl.component("composition", "composition::Listener", "comp_listener")
+    with bl.compose("my_composer") as composer:
+        # We load the first component immediately
+        bl.component("composition", "composition::Talker", "comp_talker")
+
+    # Since we exited the composer context, we can't use bl.component anymore,
+    # but we can still use the composer later
+    comp2 = Component(
+        composer, 
+        "composition", 
+        "composition::Listener", 
+        "comp_listener"
+    )
+    comp2.start()
+    # composer.load_component(comp2) would also work
