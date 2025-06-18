d05f738b18c23907a63e0ae3e5b77ab76056ae32
Nikolas Dahn <nikolas.dahn@dfki.de>
2025-06-18 17:39:31 +0200

    Added description to convenience example launch file

--- examples/09_convenience.launch.py
+++ examples/09_convenience.launch.py
@@ -3,9 +3,14 @@ from better_launch import BetterLaunch, launch_this, convenience
 
 
 @launch_this
-def so_comfortable():
+def so_comfortable(robot_description: str = None, urdf: str = None):
     """
-    
+    The convenience module provides shortcuts for a couple of tasks that are common enough to be nice to have, yet too specific to include in the main API.
     """
+    # Still need to instantiate BetterLaunch first
     bl = BetterLaunch()
+    
+    if robot_description and urdf:
+        convenience.robot_state_publisher(robot_description, urdf)
+
     convenience.rviz()
