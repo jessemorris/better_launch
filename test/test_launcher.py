5dc0ba5754618ba39375d130586f2a111886ba93
Nikolas Dahn <nikolas.dahn@dfki.de>
2025-07-22 13:33:40 +0200

    All unittests now running

--- test/test_launcher.py
+++ test/test_launcher.py
@@ -20,7 +20,7 @@ class TestBetterLaunchExamples(unittest.TestCase):
     def __init__(self, methodName="runTest"):
         super().__init__(methodName)
 
-    def assertTalkerListenerRunning(self, talker: Node, listener: Node, topic: str = "/test/better_launch/chatter") -> bool:
+    def assertTalkerListenerRunning(self, talker: Node, listener: Node, topic: str) -> bool:
         time.sleep(2.0)
 
         self.assertTrue(talker.is_running)
@@ -33,7 +33,7 @@ class TestBetterLaunchExamples(unittest.TestCase):
         )
         self.assertIn(
             topic,
-            talker.get_subscribed_topics(),
+            listener.get_subscribed_topics(),
             "Listener is not subscribed on expected topic",
         )
 
@@ -64,12 +64,12 @@ class TestBetterLaunchExamples(unittest.TestCase):
             message_future.set_result(msg)
 
         msg_type = bl.get_ros_message_type("std_msgs/msg/String")
-        sub = bl.subscriber("/test/better_launch/string", msg_type, callback=on_msg, qos_profile=qos_profile_parameters)
+        sub = bl.subscriber("/test/better_launch/topic_test", msg_type, callback=on_msg, qos_profile=qos_profile_parameters)
         
         time.sleep(1.0)
         
         bl.publish_message(
-            "/test/better_launch/string", msg_type, {"data": "hello world"}, qos_profile=qos_profile_parameters
+            "/test/better_launch/topic_test", msg_type, {"data": "hello world"}, qos_profile=qos_profile_parameters
         )
 
         result = message_future.result(2.0).data
@@ -84,19 +84,19 @@ class TestBetterLaunchExamples(unittest.TestCase):
         talker = bl.node(
             "examples_rclpy_minimal_publisher",
             "publisher_local_function",
-            "my_talker",
-            remaps={"topic": "/test/better_launch/chatter"},
+            "my_talker_node",
+            remaps={"/topic": "/test/better_launch/chatter_node"},
         )
         listener = bl.node(
             "examples_rclpy_minimal_subscriber",
             "subscriber_member_function",
-            "my_listener",
-            remaps={"topic": "/test/better_launch/chatter"},
+            "my_listener_node",
+            remaps={"/topic": "/test/better_launch/chatter_node"},
         )
 
         talker.start()
         listener.start()
-        self.assertTalkerListenerRunning(talker, listener)
+        self.assertTalkerListenerRunning(talker, listener, "/test/better_launch/chatter_node")
 
     def test_compose(self):
         bl = BetterLaunch()
@@ -105,8 +105,8 @@ class TestBetterLaunchExamples(unittest.TestCase):
             talker = bl.component(
                 "composition",
                 "composition::Talker",
-                "comp_talker",
-                remaps={"chatter": "/test/better_launch/chatter"},
+                "my_talker_comp",
+                remaps={"/chatter": "/test/better_launch/chatter_comp"},
             )
 
         # bl.compose returns a Composer that we could reuse, but even without it's possible to reuse an
@@ -115,11 +115,11 @@ class TestBetterLaunchExamples(unittest.TestCase):
             listener = bl.component(
                 package="composition",
                 plugin="composition::Listener",
-                name="comp_listener",
-                remaps={"chatter": "/test/better_launch/chatter"},
+                name="my_listener_comp",
+                remaps={"/chatter": "/test/better_launch/chatter_comp"},
             )
 
-        self.assertTalkerListenerRunning(talker, listener)
+        self.assertTalkerListenerRunning(talker, listener, "/test/better_launch/chatter_comp")
 
         composer.shutdown("Test successful", timeout=None)
         self.assertFalse(composer.is_running, "Composer failed to shutdown")
@@ -136,7 +136,7 @@ class TestBetterLaunchExamples(unittest.TestCase):
         self.assertIsNotNone(listener)
 
         # Included example doesn't use remaps
-        self.assertTalkerListenerRunning(talker, listener, "topic")
+        self.assertTalkerListenerRunning(talker, listener, "/topic")
 
     def test_ros2_actions(self):
         bl = BetterLaunch()
@@ -145,33 +145,33 @@ class TestBetterLaunchExamples(unittest.TestCase):
             RosLaunchNode(
                 package="examples_rclpy_minimal_publisher",
                 executable="publisher_local_function",
-                name="my_talker",
-                remappings=[("topic", "/test/better_launch/chatter")],
+                name="my_talker_ros2",
+                remappings=[("topic", "/test/better_launch/chatter_ros2")],
             ),
             RosLaunchNode(
                 package="examples_rclpy_minimal_subscriber",
                 executable="subscriber_member_function",
-                name="my_listener",
-                remappings=[("topic", "/test/better_launch/chatter")],
+                name="my_listener_ros2",
+                remappings=[("topic", "/test/better_launch/chatter_ros2")],
             ),
         )
 
-        time.sleep(2.0)
+        time.sleep(5.0)
 
         publishers = bl.shared_node.get_publishers_info_by_topic(
-            "/test/better_launch/chatter"
+            "/test/better_launch/chatter_ros2"
         )
         self.assertIn(
-            "my_talker",
+            "my_talker_ros2",
             [p.node_name for p in publishers],
             "Talker is not publishing on expected topic",
         )
 
         subscribers = bl.shared_node.get_subscriptions_info_by_topic(
-            "/test/better_launch/chatter"
+            "/test/better_launch/chatter_ros2"
         )
         self.assertIn(
-            "my_listener",
+            "my_listener_ros2",
             [s.node_name for s in subscribers],
             "Listener is not listening on expected topic",
         )
